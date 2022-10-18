c3a4c55aa634a7a70b5929e6a501f0fd2bd65e56
shraddha7502 <114468084+shraddha7502@users.noreply.github.com>
2022-10-18 16:09:59 +0530

    Create Podium Finish.cpp

---
+++ Codechef/Podium Finish.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int A , B;
+	    cin>>A>>B;
+	   int C= A + B; 
+	   cout<<C<<endl;
+	}
+	
+	return 0;
+}
