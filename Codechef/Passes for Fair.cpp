a5ecf510844f4a5485c9e0a83d5634ff52ae8616
shraddha7502 <114468084+shraddha7502@users.noreply.github.com>
2022-10-18 16:11:30 +0530

    Create Passes for Fair.cpp

---
+++ Codechef/Passes for Fair.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int N,K;
+        cin>>N>>K;
+        if(K>N) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+            
+
+            
+    }
+	// your code goes here
+	return 0;
+}
